d780afe620ebb9041f541107911d8fb6bb14262a
rncbc <rncbc@304e7ffa-9037-4501-99c0-529757c9bbcc>
2005-08-24 21:56:39 +0000

    This commit was generated by cvs2svn to compensate for changes in r16, which included commits to RCS files with non-trunk default branches.
    git-svn-id: http://svn.code.sf.net/p/qtractor/code_attic/trunk@17 304e7ffa-9037-4501-99c0-529757c9bbcc

---
+++ src/qtractorAbout.h
@@ -0,0 +1,36 @@
+// qtractorAbout.h
+//
+/****************************************************************************
+   Copyright (C) 2004-2005, rncbc aka Rui Nuno Capela. All rights reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+*****************************************************************************/
+
+#ifndef __qtractorAbout_h
+#define __qtractorAbout_h
+
+#include "config.h"
+
+#define QTRACTOR_TITLE        PACKAGE_NAME
+#define QTRACTOR_VERSION      PACKAGE_VERSION
+
+#define QTRACTOR_SUBTITLE     "A MIDI/Audio multi-track sequencer"
+#define QTRACTOR_WEBSITE      "http://qtractor.sourceforge.net"
+#define QTRACTOR_COPYRIGHT    "Copyright (C) 2005, rncbc aka Rui Nuno Capela. All rights reserved."
+
+#endif  // __qtractorAbout_h
+
+// end of qtractorAbout.h
---
+++ win32/config.h
@@ -0,0 +1,10 @@
+
+#define PACKAGE_NAME    "Qtractor"
+#define PACKAGE_VERSION "0.0.1"
+
+#define CONFIG_PREFIX   "."
+#define CONFIG_DEBUG    1
+
+#undef  HAVE_SIGNAL_H
+
+
