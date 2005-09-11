9cbd425cdb9805e135ce775814722958fa9f5c29
rncbc <rncbc@304e7ffa-9037-4501-99c0-529757c9bbcc>
2005-09-11 20:48:31 +0000

    This commit was generated by cvs2svn to compensate for changes in r18, which included commits to RCS files with non-trunk default branches.
    git-svn-id: http://svn.code.sf.net/p/qtractor/code_attic/trunk@19 304e7ffa-9037-4501-99c0-529757c9bbcc

---
+++ src/qtractorDocument.h
@@ -0,0 +1,70 @@
+// qtractorDocument.h
+//
+/****************************************************************************
+   Copyright (C) 2005, rncbc aka Rui Nuno Capela. All rights reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+*****************************************************************************/
+
+#ifndef __qtractorDocument_h
+#define __qtractorDocument_h
+
+#include <qdom.h>
+
+
+//-------------------------------------------------------------------------
+// qtractorDocument -- Document file import/export abstract class.
+//
+
+class qtractorDocument
+{
+public:
+
+	// Constructor.
+	qtractorDocument(QDomDocument *pDocument,
+		const QString& sTagName = QString::null);
+	// Default destructor.
+	virtual ~qtractorDocument();
+
+	// Accessors.
+	QDomDocument *document() const;
+
+	// Helper methods.
+	bool    boolFromText (const QString& s) const;
+	QString textFromBool (bool b) const;
+
+	void saveTextElement (const QString& sTagName, const QString& sText,
+		QDomElement *pElement);
+
+	// External storage simple methods.
+	bool load (const QString& sFilename);
+	bool save (const QString& sFilename);
+
+	// External storage element pure virtual methods.
+	virtual bool loadElement (QDomElement *pElement) = 0;
+	virtual bool saveElement (QDomElement *pElement) = 0;
+
+private:
+
+	// Instance variables.
+	QDomDocument *m_pDocument;
+	QString m_sTagName;
+};
+
+
+#endif  // __qtractorDocument_h
+
+// end of qtractorDocument.h
