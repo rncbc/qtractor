f1a0e06bcb1d7366ac2e2b4ce2822071948510ca
rncbc <rncbc@304e7ffa-9037-4501-99c0-529757c9bbcc>
2005-11-19 00:35:46 +0000

    This commit was generated by cvs2svn to compensate for changes in r87, which included commits to RCS files with non-trunk default branches.
    git-svn-id: http://svn.code.sf.net/p/qtractor/code_attic/trunk@88 304e7ffa-9037-4501-99c0-529757c9bbcc

---
+++ src/qtractorMidiClip.cpp
@@ -0,0 +1,313 @@
+// qtractorMidiClip.cpp
+//
+/****************************************************************************
+   Copyright (C) 2005, rncbc aka Rui Nuno Capela. All rights reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+*****************************************************************************/
+
+#include "qtractorAbout.h"
+#include "qtractorMidiClip.h"
+#include "qtractorMidiFile.h"
+#include "qtractorMidiEngine.h"
+
+#include "qtractorSessionDocument.h"
+
+#include <qfileinfo.h>
+#include <qpainter.h>
+
+
+//----------------------------------------------------------------------
+// class qtractorMidiClip -- Audio file/buffer clip.
+//
+
+// Constructor.
+qtractorMidiClip::qtractorMidiClip ( qtractorTrack *pTrack )
+	: qtractorClip(pTrack)
+{
+	m_pSeq = new qtractorMidiSequence();
+	m_iTrackChannel = 0;
+}
+
+
+// Destructor.
+qtractorMidiClip::~qtractorMidiClip (void)
+{
+	if (m_pSeq)
+		delete m_pSeq;
+}
+
+
+// The main use method.
+bool qtractorMidiClip::open ( const QString& sFilename, int iTrackChannel )
+{
+	// Open up the MIDI file...
+	qtractorMidiFile file;
+	if (!file.open(sFilename))
+		return false;
+
+	return open(&file, iTrackChannel);
+}
+
+
+// Overloaded open method; reuse an already open MIDI file.
+bool qtractorMidiClip::open ( qtractorMidiFile *pMidiFile, int iTrackChannel )
+{
+	if (track() == NULL)
+		return false;
+
+	qtractorSession *pSession = track()->session();
+	if (pSession == NULL)
+		return false;
+
+	m_pSeq->clear();
+	m_pSeq->setTicksPerBeat(pSession->ticksPerBeat());
+
+	// Read the event sequence in...
+	if (!pMidiFile->readTrack(m_pSeq, iTrackChannel))
+		return false;
+	// We must have events, otherwise this clip is of now use...
+	if (m_pSeq->events().count() < 1)
+	    return false;
+
+	// Set local properties...
+	m_sFilename = pMidiFile->filename();
+	m_iTrackChannel = iTrackChannel;
+
+	// Clip name should be clear about it all.
+	qtractorClip::setClipName(m_pSeq->name());
+	// Clip length will be whole sequence duration.
+	qtractorClip::setClipLength(pSession->frameFromTick(m_pSeq->duration()));
+
+	// Uh oh...
+	m_clipCursor.reset(m_pSeq);
+
+	return true;
+}
+
+
+// Intra-clip tick/time positioning seek.
+void qtractorMidiClip::ClipCursor::seek ( qtractorMidiSequence *pSeq,
+	unsigned long tick )
+{
+	if (time < tick) {
+		// Seek forward...
+		if (event == NULL)
+			event = pSeq->events().first();
+		while (event && event->time() < tick)
+			event = event->next();
+		time = tick;
+	}
+	else
+	if (time > tick) {
+		// Seek backward...
+		if (event == NULL)
+			event = pSeq->events().last();
+		while (event && event->time() > tick)
+			event = event->prev();
+		time = tick;
+	}
+}
+
+
+// Sequence properties accessors.
+unsigned short qtractorMidiClip::channel (void) const
+{
+	return m_pSeq->channel();
+}
+
+
+// Intra-clip tick/time positioning reset.
+void qtractorMidiClip::ClipCursor::reset ( qtractorMidiSequence *pSeq )
+{
+	event = pSeq->events().first();
+	time  = 0;
+//	if (event)
+//		time += event->time();
+}
+
+
+// Intra-clip playback frame positioning.
+void qtractorMidiClip::seek ( unsigned long iOffset )
+{
+#ifdef CONFIG_DEBUG_0
+	fprintf(stderr, "qtractorMidiClip::seek(%p, %lu)\n", this, iOffset);
+#endif
+
+	qtractorSession *pSession = track()->session();
+	if (pSession == NULL)
+		return;
+
+	// Seek for the nearest sequence event...
+	unsigned long iTimeSeek = pSession->tickFromFrame(iOffset);
+	if (iTimeSeek > 0) {
+		m_clipCursor.seek(m_pSeq, iTimeSeek);
+	} else {
+		m_clipCursor.reset(m_pSeq);
+	}
+}
+
+
+// Reset clip state.
+void qtractorMidiClip::reset (void)
+{
+#ifdef CONFIG_DEBUG_0
+	fprintf(stderr, "qtractorMidiClip::reset(%p)\n", this);
+#endif
+
+	// Reset to the first sequence event...
+	m_clipCursor.reset(m_pSeq);
+}
+
+
+// Audio clip special process cycle executive.
+void qtractorMidiClip::process ( float fGain,
+	unsigned long iFrameStart, unsigned long iFrameEnd )
+{
+#ifdef CONFIG_DEBUG_0
+	fprintf(stderr, "qtractorMidiClip::process(%p, %g, %lu, %lu)\n",
+		this, fGain, iFrameStart, iFrameEnd);
+#endif
+
+	qtractorSession *pSession = track()->session();
+	if (pSession == NULL)
+		return;
+
+	// Enqueue the requested events...
+	unsigned long iTimeClip  = pSession->tickFromFrame(clipStart());
+	unsigned long iTimeStart = pSession->tickFromFrame(iFrameStart);
+	unsigned long iTimeEnd   = pSession->tickFromFrame(iFrameEnd);
+
+	qtractorMidiEvent *pEvent = m_clipCursor.event;
+	while (pEvent) {
+		unsigned long iTimeEvent = iTimeClip + pEvent->time();
+		if (iTimeEvent > iTimeEnd)
+			break;
+		if (iTimeEvent >= iTimeStart) {
+			pSession->midiEngine()->enqueue(track(),
+				pEvent, iTimeEvent, fGain);
+		}
+		pEvent = pEvent->next();
+	}
+
+	if (pEvent)
+		m_clipCursor.time = pEvent->time();
+}
+
+
+// Audio clip paint method.
+void qtractorMidiClip::drawClip ( QPainter *pPainter, const QRect& clipRect,
+	unsigned long iClipOffset )
+{
+	qtractorSession *pSession = track()->session();
+	if (pSession == NULL)
+		return;
+
+	// Fill clip background...
+#if 0
+	pPainter->fillRect(clipRect, track()->background());
+#else
+	pPainter->setPen(track()->background().dark());
+	pPainter->setBrush(track()->background());
+	pPainter->drawRect(clipRect);
+#endif
+
+	// Check maximum note span...
+	int iNoteSpan = (m_pSeq->noteMax() - m_pSeq->noteMin());
+	if (iNoteSpan < 1)
+		return;
+
+	unsigned long iTimeStart = pSession->tickFromFrame(iClipOffset);
+	unsigned long iTimeEnd
+		= iTimeStart + pSession->tickFromPixel(clipRect.width());
+
+	const QColor fg = track()->foreground();
+	pPainter->setPen(fg);
+	pPainter->setBrush(fg.light());
+
+	int cx = pSession->pixelFromTick(iTimeStart);
+	int h1 = clipRect.height() - 2;
+	int n1 = iNoteSpan + 1;
+	int h  = h1 / n1;
+	if (h < 4) h = 4;
+
+	qtractorMidiEvent *pEvent = m_pSeq->events().first();
+	while (pEvent && pEvent->time() < iTimeEnd) {
+		if (pEvent->type() == qtractorMidiEvent::NOTEON
+			&& pEvent->time() + pEvent->duration() >= iTimeStart) {
+			int x = clipRect.x()
+				+ pSession->pixelFromTick(pEvent->time()) - cx;
+			int y = clipRect.bottom()
+				- (h1 * (pEvent->note() - m_pSeq->noteMin() + 1)) / n1;
+			int w = pSession->pixelFromTick(pEvent->duration());
+			if (h > 4) {
+				if (w < 5) w = 5;
+				pPainter->fillRect(x, y, w, h - 1, fg);
+				pPainter->fillRect(x + 1, y + 1, w - 4, h - 4, fg.light());
+			} else {
+				if (w < 3) w = 3;
+				pPainter->drawRect(x, y, w, h - 1);
+			}
+		}
+		pEvent = pEvent->next();
+	}
+}
+
+
+// Virtual document element methods.
+bool qtractorMidiClip::loadClipElement (
+	qtractorSessionDocument * /* pDocument */, QDomElement *pElement )
+{
+	QString sFilename;
+	unsigned short iTrackChannel = 0;
+
+	// Load track children...
+	for (QDomNode nChild = pElement->firstChild();
+			!nChild.isNull();
+				nChild = nChild.nextSibling()) {
+		// Convert node to element...
+		QDomElement eChild = nChild.toElement();
+		if (eChild.isNull())
+			continue;
+		// Load track state..
+		if (eChild.tagName() == "filename")
+			sFilename = eChild.text();
+		else if (eChild.tagName() == "track-channel")
+			iTrackChannel = eChild.text().toUShort();
+	}
+
+	if (sFilename.isEmpty())
+		return false;
+
+	return qtractorMidiClip::open(sFilename, iTrackChannel);
+}
+
+
+bool qtractorMidiClip::saveClipElement (
+	qtractorSessionDocument *pDocument, QDomElement *pElement )
+{
+	QDomElement eMidiClip = pDocument->document()->createElement("midi-clip");
+	pDocument->saveTextElement("filename",
+		m_sFilename, &eMidiClip);
+	pDocument->saveTextElement("track-channel",
+		QString::number(m_iTrackChannel), &eMidiClip);
+	pElement->appendChild(eMidiClip);
+
+	return true;
+}
+
+
+// end of qtractorMidiClip.cpp
---
+++ src/qtractorMidiEngine.h
@@ -0,0 +1,122 @@
+// qtractorMidiEngine.h
+//
+/****************************************************************************
+   Copyright (C) 2005, rncbc aka Rui Nuno Capela. All rights reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+*****************************************************************************/
+
+#ifndef __qtractorMidiEngine_h
+#define __qtractorMidiEngine_h
+
+#include "qtractorEngine.h"
+
+#include <alsa/asoundlib.h>
+
+// Forward declarations.
+class qtractorMidiBus;
+class qtractorMidiEvent;
+class qtractorMidiOutputThread;
+
+
+//----------------------------------------------------------------------
+// class qtractorMidiEngine -- ALSA sequencer client instance (singleton).
+//
+
+class qtractorMidiEngine : public qtractorEngine
+{
+public:
+
+	// Constructor.
+	qtractorMidiEngine(qtractorSession *pSession);
+
+	// ALSA client descriptor accessor.
+	snd_seq_t *alsaSeq() const;
+	int alsaClient() const;
+	int alsaQueue() const;
+
+	// Special slave sync method.
+	void sync();
+
+	// MIDI event enqueue method.
+	void enqueue(qtractorTrack *pTrack, qtractorMidiEvent *pEvent,
+		unsigned long iTime, float fGain = 1.0);
+
+	// Flush ouput queue (if necessary)...
+	void flush();
+
+	// Special track-immediate methods.
+	void trackMute(qtractorTrack *pTrack, bool bMute);
+
+	// Document element methods.
+	bool loadElement(qtractorSessionDocument *pDocument,
+		QDomElement *pElement);
+	bool saveElement(qtractorSessionDocument *pDocument,
+		QDomElement *pElement);
+
+protected:
+
+	// Concrete device (de)activation methods.
+	bool init(const QString& sClientName);
+	bool activate();
+	bool start();
+	void stop();
+	void deactivate();
+	void clean();
+
+private:
+
+	// Device instance variables.
+	snd_seq_t *m_pAlsaSeq;
+	int        m_iAlsaClient;
+	int        m_iAlsaQueue;
+	
+	// Name says it all.
+	qtractorMidiOutputThread *m_pOutputThread;
+	// The time when playback started.
+	unsigned long m_iTimeStart;
+};
+
+
+//----------------------------------------------------------------------
+// class qtractorMidiBus -- Managed ALSA sequencer port set
+//
+
+class qtractorMidiBus : public qtractorBus
+{
+public:
+
+	// Constructor.
+	qtractorMidiBus(const QString& sBusName, BusMode mode = Duplex);
+
+	// ALSA sequencer port accessor.
+	int alsaPort() const;
+
+	// Activation methods.
+	bool open();
+	void close();
+
+private:
+
+	// Instance variables.
+	int m_iAlsaPort;
+};
+
+
+#endif  // __qtractorMidiEngine_h
+
+
+// end of qtractorMidiEngine.h
