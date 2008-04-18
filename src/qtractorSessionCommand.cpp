89203bb4242ae3f7b74332c08d3855ed7ffe2b20
rncbc <rncbc@304e7ffa-9037-4501-99c0-529757c9bbcc>
2008-04-18 23:32:34 +0000

    - Another leftover fixed due to recent time-stretch semantics   inversion, this time affecting the automatic time-stretching   option on changing session tempo.
    git-svn-id: http://svn.code.sf.net/p/qtractor/code_attic/trunk@884 304e7ffa-9037-4501-99c0-529757c9bbcc

--- src/qtractorSessionCommand.cpp
+++ src/qtractorSessionCommand.cpp
@@ -75,8 +75,8 @@ qtractorSessionTempoCommand::qtractorSessionTempoCommand (
 					if (pAudioClip) {
 						if (m_pClipCommand == NULL)
 							m_pClipCommand = new qtractorClipCommand(name());
-						float fTimeStretch = m_fTempo
-							/ (pSession->tempo() * pAudioClip->timeStretch());
+						float fTimeStretch = (pSession->tempo()
+							* pAudioClip->timeStretch()) / m_fTempo;
 						m_pClipCommand->timeStretchClip(pClip, fTimeStretch);
 					}
 				}
