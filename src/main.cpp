ab907841f9163a1b07cfe6215e24d15e697ab79b
rncbc <rncbc@304e7ffa-9037-4501-99c0-529757c9bbcc>
2008-05-21 16:01:39 +0000

    - Attempt to load Qt's own translation support and get rid of   the ever warning startup message, unless built in debug mode.   (transaction by Guido Scholz, while on qsynth-devel, thanks).
    - Only one application instance is now allowed to be up and running,
      with immediate but graceful termination upon startup iif an already
      running instance is detected, which will see its main widget shown
      up automatically (Qt/X11 platform only).
    
    
    git-svn-id: http://svn.code.sf.net/p/qtractor/code_attic/trunk@915 304e7ffa-9037-4501-99c0-529757c9bbcc

--- src/main.cpp
+++ src/main.cpp
@@ -24,14 +24,204 @@
 #include "qtractorMainForm.h"
 
 #include <QApplication>
+#include <QLibraryInfo>
 #include <QTranslator>
 #include <QLocale>
 
-#ifdef CONFIG_XINITTHREADS
 #if defined(Q_WS_X11)
+
+//-------------------------------------------------------------------------
+// Single application instance stuff (Qt/X11 only atm.)
+//
+
+#include <QX11Info>
+
+#include <X11/Xatom.h>
 #include <X11/Xlib.h>
+
+#define QTRACTOR_XUNIQUE "qtractorMainForm_xunique"
+
 #endif
+
+class qtractorApplication : public QApplication
+{
+public:
+
+	// Constructor.
+	qtractorApplication(int& argc, char **argv) : QApplication(argc, argv),
+		m_pQtTranslator(0), m_pMyTranslator(0), m_pWidget(0)	
+	{
+		// Load translation support.
+		QLocale loc;
+		if (loc.language() != QLocale::C) {
+			// Try own Qt translation...
+			m_pQtTranslator = new QTranslator(this);
+			QString sLocName = "qt_" + loc.name();
+			QString sLocPath = QLibraryInfo::location(QLibraryInfo::TranslationsPath);
+			if (m_pQtTranslator->load(sLocName, sLocPath)) {
+				QApplication::installTranslator(m_pQtTranslator);
+			} else {
+				delete m_pQtTranslator;
+				m_pQtTranslator = 0;
+		#ifdef CONFIG_DEBUG
+				qWarning("Warning: no translation found for '%s' locale: %s/%s.qm",
+					loc.name().toUtf8().constData(),
+					sLocPath.toUtf8().constData(),
+					sLocName.toUtf8().constData());
+		#endif
+			}
+			// Try own application translation...
+			m_pMyTranslator = new QTranslator(this);
+			sLocName = "qtractor_" + loc.name();
+			if (m_pMyTranslator->load(sLocName, sLocPath)) {
+				QApplication::installTranslator(m_pMyTranslator);
+			} else {
+				sLocPath = CONFIG_PREFIX "/share/locale";
+				if (m_pMyTranslator->load(sLocName, sLocPath)) {
+					QApplication::installTranslator(m_pMyTranslator);
+				} else {
+					delete m_pMyTranslator;
+					m_pMyTranslator = 0;
+		#ifdef CONFIG_DEBUG
+					qWarning("Warning: no translation found for '%s' locale: %s/%s.qm",
+						loc.name().toUtf8().constData(),
+						sLocPath.toUtf8().constData(),
+						sLocName.toUtf8().constData());
+		#endif
+				}
+			}
+		}
+	#if defined(Q_WS_X11)
+		m_pDisplay = QX11Info::display();
+		m_aUnique  = XInternAtom(m_pDisplay, QTRACTOR_XUNIQUE, false);
+		XGrabServer(m_pDisplay);
+		m_wOwner = XGetSelectionOwner(m_pDisplay, m_aUnique);
+		XUngrabServer(m_pDisplay);
+	#endif
+	}
+
+	// Destructor.
+	~qtractorApplication()
+	{
+		if (m_pMyTranslator) delete m_pMyTranslator;
+		if (m_pQtTranslator) delete m_pQtTranslator;
+	}
+
+	// Main application widget accessors.
+	void setMainWidget(QWidget *pWidget)
+	{
+		m_pWidget = pWidget;
+	#if defined(Q_WS_X11)
+		XGrabServer(m_pDisplay);
+		m_wOwner = m_pWidget->winId();
+		XSetSelectionOwner(m_pDisplay, m_aUnique, m_wOwner, CurrentTime);
+		XUngrabServer(m_pDisplay);
+	#endif
+	}
+
+	QWidget *mainWidget() const { return m_pWidget; }
+
+	// Check if another instance is running,
+    // and raise its proper main widget...
+	bool setup()
+	{
+	#if defined(Q_WS_X11)
+		if (m_wOwner != None) {
+			// First, notify any freedesktop.org WM
+			// that we're about to show the main widget...
+			Screen *pScreen = XDefaultScreenOfDisplay(m_pDisplay);
+			int iScreen = XScreenNumberOfScreen(pScreen);
+			XEvent ev;
+			memset(&ev, 0, sizeof(ev));
+			ev.xclient.type = ClientMessage;
+			ev.xclient.display = m_pDisplay;
+			ev.xclient.window = m_wOwner;
+			ev.xclient.message_type = XInternAtom(m_pDisplay, "_NET_ACTIVE_WINDOW", false);
+			ev.xclient.format = 32;
+			ev.xclient.data.l[0] = 0; // Source indication.
+			ev.xclient.data.l[1] = 0; // Timestamp.
+			ev.xclient.data.l[2] = 0; // Requestor's currently active window (none)
+			ev.xclient.data.l[3] = 0;
+			ev.xclient.data.l[4] = 0;
+			XSelectInput(m_pDisplay, m_wOwner, StructureNotifyMask);
+			XSendEvent(m_pDisplay, RootWindow(m_pDisplay, iScreen), false,
+				(SubstructureNotifyMask | SubstructureRedirectMask), &ev);
+			XSync(m_pDisplay, false);
+			XRaiseWindow(m_pDisplay, m_wOwner);
+			// And then, let it get caught on destination
+			// by QApplication::x11EventFilter...
+			QByteArray value = QTRACTOR_XUNIQUE;
+			XChangeProperty(
+				m_pDisplay,
+				m_wOwner,
+				m_aUnique,
+				m_aUnique, 8,
+				PropModeReplace,
+				(unsigned char *) value.data(),
+				value.length());
+			// Done.
+			return true;
+		}
+	#endif
+		return false;
+	}
+
+#if defined(Q_WS_X11)
+	bool x11EventFilter(XEvent *pEv)
+	{
+		if (m_pWidget && m_wOwner != None
+			&& pEv->type == PropertyNotify
+			&& pEv->xproperty.window == m_wOwner
+			&& pEv->xproperty.state == PropertyNewValue) {
+			// Always check whether our property-flag is still around...
+			Atom aType;
+			int iFormat = 0;
+			unsigned long iItems = 0;
+			unsigned long iAfter = 0;
+			unsigned char *pData = 0;
+			if (XGetWindowProperty(
+					m_pDisplay,
+					m_wOwner,
+					m_aUnique,
+					0, 1024,
+					false,
+					m_aUnique,
+					&aType,
+					&iFormat,
+					&iItems,
+					&iAfter,
+					&pData) == Success
+				&& aType == m_aUnique && iItems > 0 && iAfter == 0) {
+				// Avoid repeating it-self...
+				XDeleteProperty(m_pDisplay, m_wOwner, m_aUnique);
+				// Just make it always shows up fine...
+				m_pWidget->show();
+				m_pWidget->raise();
+				m_pWidget->activateWindow();
+			}
+			// Free any left-overs...
+			if (iItems > 0 && pData)
+				XFree(pData);
+		}
+		return QApplication::x11EventFilter(pEv);
+	}
 #endif
+	
+private:
+
+	// Translation support.
+	QTranslator *m_pQtTranslator;
+	QTranslator *m_pMyTranslator;
+
+	// Instance variables.
+	QWidget *m_pWidget;
+
+#if defined(Q_WS_X11)
+	Display *m_pDisplay;
+	Atom     m_aUnique;
+	Window   m_wOwner;
+#endif
+};
 
 
 //-------------------------------------------------------------------------
@@ -43,27 +233,12 @@ int main ( int argc, char **argv )
 #ifdef CONFIG_XINITTHREADS
 #if defined(Q_WS_X11)
 	if (!XInitThreads()) {
-		fprintf(stderr, "Error: XInitThreads() failed. Stop.\n");
+		qError("Error: XInitThreads() failed. Stop.");
 		return 1;
 	}
 #endif
 #endif
-	QApplication app(argc, argv);
-
-	// Load translation support.
-	QTranslator translator(0);
-	QLocale loc;
-	if (loc.language() != QLocale::C) {
-		QString sLocName = "qtractor_" + loc.name();
-		if (!translator.load(sLocName, ".")) {
-			QString sLocPath = CONFIG_PREFIX "/share/locale";
-			if (!translator.load(sLocName, sLocPath))
-				fprintf(stderr, "Warning: no locale found: %s/%s.qm\n",
-					sLocPath.toUtf8().constData(),
-					sLocName.toUtf8().constData());
-		}
-		app.installTranslator(&translator);
-	}
+	qtractorApplication app(argc, argv);
 
 	// Construct default settings; override with command line arguments.
 	qtractorOptions options;
@@ -72,11 +247,20 @@ int main ( int argc, char **argv )
 		return 1;
 	}
 
+	// Have another instance running?
+	if (app.setup()) {
+		app.quit();
+		return 2;
+	}
+
 	// Construct, setup and show the main form (a pseudo-singleton).
 	qtractorMainForm w;
 //	app.setMainWidget(&w);
 	w.setOptions(&options);
 	w.show();
+
+	// Settle this one as application main widget...
+	app.setMainWidget(&w);
 
 	// Register the quit signal/slot.
 	// app.connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
