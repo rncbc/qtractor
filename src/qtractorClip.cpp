60bf60f065cb7d859f87709b74af06d8414c5405
rncbc <rncbc@304e7ffa-9037-4501-99c0-529757c9bbcc>
2009-04-01 18:52:41 +0000

    - Internal MIDI editor cursor reset refactoring (infra-)fix.
    git-svn-id: http://svn.code.sf.net/p/qtractor/code_attic/trunk@1269 304e7ffa-9037-4501-99c0-529757c9bbcc

--- src/qtractorClip.cpp
+++ src/qtractorClip.cpp
@@ -1,7 +1,7 @@
 // qtractorClip.cpp
 //
 /****************************************************************************
-   Copyright (C) 2005-2008, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2005-2009, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -549,6 +549,12 @@ bool qtractorClip::startEditor ( QWidget *pParent )
 bool qtractorClip::queryEditor (void)
 {
 	return !isDirty();
+}
+
+// Clip editor reset.
+void qtractorClip::resetEditor (void)
+{
+	// Do nothing here.
 }
 
 // Clip editor update.
