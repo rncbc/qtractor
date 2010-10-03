8cc804f6a0963fe943dfe888317643ea6deffcbe
rncbc <rncbc@304e7ffa-9037-4501-99c0-529757c9bbcc>
2010-10-03 17:14:00 +0000

    - Make it build for Qt < 4.6 ...fix.
    git-svn-id: http://svn.code.sf.net/p/qtractor/code_attic/trunk@1689 304e7ffa-9037-4501-99c0-529757c9bbcc

--- src/qtractorMainForm.cpp
+++ src/qtractorMainForm.cpp
@@ -353,7 +353,7 @@ qtractorMainForm::qtractorMainForm (
 	// Editable toolbar widgets special palette.
 	QPalette pal;
 	// Outrageous HACK: GTK+ ppl won't see green on black thing...
-#if defined(Q_WS_X11) && QT_VERSION >= 0x040600
+#if defined(Q_WS_X11) && QT_VERSION >= 0x040500
 	if (qobject_cast<QGtkStyle *> (style()) == NULL) {
 #endif
 	//	pal.setColor(QPalette::Window, Qt::black);
