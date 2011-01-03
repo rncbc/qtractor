ad440da8314bf005e78797b8afc2011b9ef1fb33
rncbc <rncbc@304e7ffa-9037-4501-99c0-529757c9bbcc>
2011-01-03 19:42:48 +0000

    - Main Edit/Clip menu slight re-arrangement.
    git-svn-id: http://svn.code.sf.net/p/qtractor/code_attic/trunk@1773 304e7ffa-9037-4501-99c0-529757c9bbcc

--- src/qtractorMidiToolsForm.cpp
+++ src/qtractorMidiToolsForm.cpp
@@ -1,7 +1,7 @@
 // qtractorMidiToolsForm.cpp
 //
 /****************************************************************************
-   Copyright (C) 2005-2010, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2005-2011, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -60,7 +60,7 @@ public:
 		//	t = ::powf(t, p);
 		else
 		if (p < 0.0f)
-			t = ::sqrtf(1.0f - ((1.0f - t) * ::powf(1.0f + (::logf(1.0f- t) / p), -p)));
+			t = ::sqrtf(1.0f - ((1.0f - t) * ::powf(1.0f + (::logf(1.0f - t) / p), -p)));
 		//	t = 1.0f - ::powf(1.0f - t, -p);
 
 		return t;
