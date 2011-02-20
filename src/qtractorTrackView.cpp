25addefb508545aea838f6af6187eb4fa4f7763b
rncbc <rncbc@304e7ffa-9037-4501-99c0-529757c9bbcc>
2011-02-20 19:09:46 +0000

    - Floating tool-tips now being shown also while   on mouse rubber-banding (drag-select). (FIX)
    git-svn-id: http://svn.code.sf.net/p/qtractor/code_attic/trunk@1856 304e7ffa-9037-4501-99c0-529757c9bbcc

--- src/qtractorTrackView.cpp
+++ src/qtractorTrackView.cpp
@@ -1353,7 +1353,7 @@ void qtractorTrackView::mouseMoveEvent ( QMouseEvent *pMouseEvent )
 		moveRubberBand(&m_pRubberBand, m_rectDrag);
 		qtractorScrollView::ensureVisible(pos.x(), pos.y(), 24, 24);
 		selectRect(m_rectDrag, m_selectMode);
-		showToolTip(m_rectDrag, m_iDraggingX);
+		showToolTip(m_rectDrag.normalized(), m_iDraggingX);
 		break;
 	case DragStart:
 		if ((m_posDrag - pos).manhattanLength()
