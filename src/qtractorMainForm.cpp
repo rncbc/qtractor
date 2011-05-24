0ea8b61c56338bebe08809059df77e0749cb8926
rncbc <rncbc@304e7ffa-9037-4501-99c0-529757c9bbcc>
2011-05-24 07:18:40 +0000

    - Session file format saved on JACK session has   been reverted to archive/zip bundle one (.qtz)   now using temporary extraction directory when   loading an existing  JACK session (REGRESSION   REVERT).
    git-svn-id: http://svn.code.sf.net/p/qtractor/code_attic/trunk@2070 304e7ffa-9037-4501-99c0-529757c9bbcc

--- src/qtractorMainForm.cpp
+++ src/qtractorMainForm.cpp
@@ -1741,7 +1741,14 @@ bool qtractorMainForm::loadSessionFileEx (
 		iFlags |= qtractorDocument::Archive;
 		// Take special precaution for already
 		// existing archive directory...
-		info.setFile(info.path() + '/' + info.completeBaseName());
+		QString sPath;
+		if (bUpdate) {
+			sPath = info.path();
+		} else {
+			sPath = QDir::temp().path();
+			iFlags |= qtractorDocument::Temporary;
+		}
+		info.setFile(sPath + QDir::separator() + info.completeBaseName());
 		if (info.exists() && info.isDir()) {
 			if (QMessageBox::warning(this,
 				tr("Warning") + " - " QTRACTOR_TITLE,
@@ -1909,11 +1916,6 @@ bool qtractorMainForm::saveSessionFileEx (
 			m_pOptions->sSessionDir = QFileInfo(sFilename).absolutePath();
 			m_pOptions->saveOptions();
 		}
-	#ifdef CONFIG_LIBZ
-		// FIXME: Is it time to cleanup extracted archives (do NOT remove)?
-		if ((iFlags & qtractorDocument::Archive) == 0)
-			qtractorDocument::clearExtractedArchives(false);
-	#endif
 	} else {
 		// Something went wrong...
 		appendMessagesError(
@@ -5253,7 +5255,7 @@ void qtractorMainForm::audioSessNotify ( void *pvSessionArg )
 	const QString sSessionFile = sSessionName + '.'
 		+ (bTemplate
 			? qtractorDocument::templateExt()
-			: qtractorDocument::defaultExt());
+			: qtractorDocument::archiveExt());
 
 	QStringList args;
 	args << QApplication::applicationFilePath();
