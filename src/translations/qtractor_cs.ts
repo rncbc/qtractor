eb4e3dc188ec695802fa642d445aefee09b46191
rncbc <rncbc@304e7ffa-9037-4501-99c0-529757c9bbcc>
2011-12-08 11:39:36 +0000

    - Fixed a probably old lurking destructive bug, which   was incidental to when an invalid or non audio file   is imported, intentionally or by mistake, into new   or existing audio clips (eg. importing a MIDI file   where an audio file is expected).
    git-svn-id: http://svn.code.sf.net/p/qtractor/code_attic/trunk@2624 304e7ffa-9037-4501-99c0-529757c9bbcc

--- src/translations/qtractor_cs.ts
+++ src/translations/qtractor_cs.ts
@@ -555,25 +555,25 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../qtractorAudioClip.cpp" line="588"/>
+        <location filename="../qtractorAudioClip.cpp" line="584"/>
         <source>
 Audio:	%1 channels, %2 Hz</source>
         <translation>Audio:	%1 kanály, %2 Hz</translation>
     </message>
     <message>
-        <location filename="../qtractorAudioClip.cpp" line="593"/>
+        <location filename="../qtractorAudioClip.cpp" line="589"/>
         <source> (%1 dB)</source>
         <translation>(%1 dB)</translation>
     </message>
     <message>
-        <location filename="../qtractorAudioClip.cpp" line="596"/>
+        <location filename="../qtractorAudioClip.cpp" line="592"/>
         <source>
 	(%1% time stretch)</source>
         <translation>
 	(%1% natažení času)</translation>
     </message>
     <message>
-        <location filename="../qtractorAudioClip.cpp" line="599"/>
+        <location filename="../qtractorAudioClip.cpp" line="595"/>
         <source>
 	(%1 semitones pitch shift)</source>
         <translation>
