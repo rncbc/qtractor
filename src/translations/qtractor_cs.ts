584c8a53572fb6456d0864ead8aa9c83298232ba
rncbc <rncbc@304e7ffa-9037-4501-99c0-529757c9bbcc>
2012-02-11 13:44:26 +0000

    - Auto-monitored MIDI events are now merged/queued correctly into the   instrument plugin playback queue, avoiding sudden crashes, hopefully.   (EXPERIMENTAL)
    git-svn-id: http://svn.code.sf.net/p/qtractor/code_attic/trunk@2704 304e7ffa-9037-4501-99c0-529757c9bbcc

--- src/translations/qtractor_cs.ts
+++ src/translations/qtractor_cs.ts
@@ -712,7 +712,7 @@ Délka:	%4</translation>
         <translation>%1 Banka %2</translation>
     </message>
     <message>
-        <location filename="../qtractorMidiBuffer.cpp" line="928"/>
+        <location filename="../qtractorMidiBuffer.cpp" line="931"/>
         <source>%1 - Bank %2</source>
         <translation>%1 - Banka %2</translation>
     </message>
