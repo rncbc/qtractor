1e73a849dce65eba65b524fbb74b1dd76ffdaa60
rncbc <rncbc@304e7ffa-9037-4501-99c0-529757c9bbcc>
2012-09-18 07:57:59 +0000

    - Regression to muting MIDI track while editing and playback is rolling.
    git-svn-id: http://svn.code.sf.net/p/qtractor/code_attic/trunk@2995 304e7ffa-9037-4501-99c0-529757c9bbcc

--- src/qtractorMidiCursor.cpp
+++ src/qtractorMidiCursor.cpp
@@ -1,7 +1,7 @@
 // qtractorMidiCursor.cpp
 //
 /****************************************************************************
-   Copyright (C) 2005-2010, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2005-2012, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -85,7 +85,6 @@ qtractorMidiEvent *qtractorMidiCursor::reset (
 		m_pEvent = pSeq->events().first();
 	// That was it...
 	m_iTime = iTime;
-	
 	return m_pEvent;
 }
 
