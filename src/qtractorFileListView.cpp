0a6076333389983b2e1c7b36197ff3c56687a6cc
rncbc <rncbc@304e7ffa-9037-4501-99c0-529757c9bbcc>
2013-01-01 19:47:00 +0000

    - Fixed an immediate null pointer crash on creating a parentless new   group while on the files organizer widget.
    git-svn-id: http://svn.code.sf.net/p/qtractor/code_attic/trunk@3121 304e7ffa-9037-4501-99c0-529757c9bbcc

--- src/qtractorFileListView.cpp
+++ src/qtractorFileListView.cpp
@@ -1,7 +1,7 @@
 // qtractorFileListView.cpp
 //
 /****************************************************************************
-   Copyright (C) 2005-2012, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2005-2013, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -553,7 +553,7 @@ void qtractorFileListView::openFile (void)
 void qtractorFileListView::newGroup (void)
 {
 	qtractorFileGroupItem *pParentItem = currentGroupItem();
-	if (!pParentItem->isOpen())
+	if (pParentItem && !pParentItem->isOpen())
 		pParentItem = pParentItem->groupItem();
 	qtractorFileGroupItem *pGroupItem
 		= addGroupItem(tr("New Group"), pParentItem);
