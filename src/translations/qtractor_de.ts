79427d2a3e06d3c1e47bdb41567c69de6e511b89
rncbc <rncbc@304e7ffa-9037-4501-99c0-529757c9bbcc>
2014-06-05 17:10:56 +0000

    - Once again, audio frame/MIDI time drift correction has been   slightly refactored to improve MIDI input monitor and timing.   (EXPERIMENTAL)
    git-svn-id: http://svn.code.sf.net/p/qtractor/code_attic/trunk@3880 304e7ffa-9037-4501-99c0-529757c9bbcc

--- src/translations/qtractor_de.ts
+++ src/translations/qtractor_de.ts
@@ -30,13 +30,13 @@ Audio:	%1 Kanäle, %2 Hz</translation>
     </message>
     <message>
         <location filename="../qtractorAudioEngine.cpp" line="2608"/>
-        <location filename="../qtractorMidiEngine.cpp" line="4262"/>
+        <location filename="../qtractorMidiEngine.cpp" line="4272"/>
         <source>%1 In</source>
         <translation>%1 Ein</translation>
     </message>
     <message>
         <location filename="../qtractorAudioEngine.cpp" line="2608"/>
-        <location filename="../qtractorMidiEngine.cpp" line="4262"/>
+        <location filename="../qtractorMidiEngine.cpp" line="4272"/>
         <source>%1 Out</source>
         <translation>%1 Aus</translation>
     </message>
