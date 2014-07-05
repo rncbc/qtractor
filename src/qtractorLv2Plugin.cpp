49bf2c234a7f783dca69c8f42252ab38488241b7
rncbc <rncbc@304e7ffa-9037-4501-99c0-529757c9bbcc>
2014-07-05 14:30:18 +0000

    - Plugin removal de-activation fix.
    git-svn-id: http://svn.code.sf.net/p/qtractor/code_attic/trunk@3953 304e7ffa-9037-4501-99c0-529757c9bbcc

--- src/qtractorLv2Plugin.cpp
+++ src/qtractorLv2Plugin.cpp
@@ -2109,7 +2109,7 @@ void qtractorLv2Plugin::setChannels ( unsigned short iChannels )
 
 	// Bail out, if none are about to be created...
 	if (iInstances < 1) {
-	//	setActivated(bActivated);
+		setActivated(bActivated);
 		return;
 	}
 
