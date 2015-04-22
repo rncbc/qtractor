2b46faba0dddb682c3ba537f0d2807993319b46e
rncbc <rncbc@gamma.rncbc.lan>
2015-04-22 17:42:32 +0100

    - Some slack given to MIDI clip note drawing... :)

--- src/translations/qtractor_ja.ts
+++ src/translations/qtractor_ja.ts
@@ -280,32 +280,32 @@ Offs. / Len.:	%3 / %4</source>
         <translation>%1 - バンク %2</translation>
     </message>
     <message>
-        <location filename="../qtractorMidiClip.cpp" line="1139"/>
+        <location filename="../qtractorMidiClip.cpp" line="1144"/>
         <source>(format %1)
 MIDI:	</source>
         <translation>(フォーマット %1)
 MIDI:	</translation>
     </message>
     <message>
-        <location filename="../qtractorMidiClip.cpp" line="1141"/>
+        <location filename="../qtractorMidiClip.cpp" line="1146"/>
         <location filename="../qtractorMidiListView.cpp" line="73"/>
         <source>Channel %1</source>
         <translation>チャンネル %1</translation>
     </message>
     <message>
-        <location filename="../qtractorMidiClip.cpp" line="1143"/>
+        <location filename="../qtractorMidiClip.cpp" line="1148"/>
         <location filename="../qtractorMidiFile.cpp" line="1237"/>
         <location filename="../qtractorMidiListView.cpp" line="67"/>
         <source>Track %1</source>
         <translation>トラック %1</translation>
     </message>
     <message>
-        <location filename="../qtractorMidiClip.cpp" line="1146"/>
+        <location filename="../qtractorMidiClip.cpp" line="1151"/>
         <source>, %1 tracks, %2 tpqn</source>
         <translation>, %1 トラック、 %2 tpqn</translation>
     </message>
     <message>
-        <location filename="../qtractorMidiClip.cpp" line="1152"/>
+        <location filename="../qtractorMidiClip.cpp" line="1157"/>
         <source> (%1% vol)</source>
         <translation> (%1% ボリューム)</translation>
     </message>
