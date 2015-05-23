d610a478e4abb738f5658cb2bcf6e41db68a21c0
rncbc <rncbc@gamma.rncbc.lan>
2015-05-23 10:07:24 +0100

    - Spanish (es) translation updated (by David Reyes Pucheta, thanks).

--- src/translations/qtractor_es.ts
+++ src/translations/qtractor_es.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="es_ES">
+<TS version="2.1" language="es_ES">
 <context>
     <name>QObject</name>
     <message>
@@ -5483,7 +5483,7 @@ la próxima vez que inicie este %1.</translation>
     <message>
         <location filename="../qtractorMainForm.cpp" line="5411"/>
         <source>LV2 Plug-in MIDI/Event support enabled.</source>
-        <translation type="unfinished"></translation>
+        <translation>Soporte de MIDI/Evento de Plug in LV2 habilitado.</translation>
     </message>
     <message>
         <source>LV2 Plug-in MIDI/Event support disabled.</source>
@@ -9343,12 +9343,12 @@ Are you sure?</source>
     <message>
         <location filename="../qtractorOptionsForm.ui" line="569"/>
         <source>Whether to start as timebase master (JACK)</source>
-        <translation type="unfinished"></translation>
+        <translation>Si se debe iniciar cómo base de tiempo maestro (JACK)</translation>
     </message>
     <message>
         <location filename="../qtractorOptionsForm.ui" line="572"/>
         <source>&amp;Timebase</source>
-        <translation type="unfinished"></translation>
+        <translation>Base de &amp;tiempo</translation>
     </message>
     <message>
         <location filename="../qtractorOptionsForm.ui" line="598"/>
