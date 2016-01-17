5fbb70bd0af88bc973fcba7ff041960c86880795
rncbc <rncbc@gamma.rncbc.lan>
2016-01-17 17:40:11 +0000

    - Fixed main time-ruler view update.

--- src/qtractorTrackView.cpp
+++ src/qtractorTrackView.cpp
@@ -316,7 +316,7 @@ void qtractorTrackView::updateContentsWidth ( int iContentsWidth )
 	// Force an update on the track time line too...
 	m_pTracks->trackTime()->resizeContents(
 		iContentsWidth + 100, m_pTracks->trackTime()->viewport()->height());
-//	m_pTracks->trackTime()->updateContents();
+	m_pTracks->trackTime()->updateContents();
 }
 
 
