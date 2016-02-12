fe51b37e05150ef59c2aa0034d838f37b4b302c4
rncbc <rncbc@t430.rncbc.lan>
2016-02-12 12:03:59 +0000

    - Stub cbrtf() optimization on MIDI controller logarithmic scale approximation.

--- src/qtractorMidiControl.cpp
+++ src/qtractorMidiControl.cpp
@@ -43,14 +43,13 @@
 #include <math.h>
 
 // Possible cube root optimization.
-// (borrowed from metamerist.com)
 static inline float cbrtf2 ( float x )
 {
-#ifdef CONFIG_FLOAT32
+#if 0//ifdef CONFIG_FLOAT32
 	// Avoid strict-aliasing optimization (gcc -O2).
 	union { float f; int i; } u;
 	u.f = x;
-	u.i = (u.i / 3) + 710235478;
+	u.i = (u.i / 3) + 710235478; // HACK: 709621077 for cbrtf2(1)=1
 	return u.f;
 #else
 	return ::cbrtf(x);
