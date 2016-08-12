8a4e0eded667245f43cbb0317e081f99d85f9dc3
rncbc <rncbc@gamma.rncbc.lan>
2016-08-13 00:16:38 +0100

    - Added unique prefix option to zip/archive entry alias. (bis)

--- src/qtractorDocument.cpp
+++ src/qtractorDocument.cpp
@@ -1,7 +1,7 @@
 // qtractorDocument.cpp
 //
 /****************************************************************************
-   Copyright (C) 2005-201, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2005-2016, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
