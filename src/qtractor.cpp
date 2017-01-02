fead8180197f33d6e1d1eb9984caed7fe4c11dcc
rncbc <rncbc@rncbc.org>
2017-01-02 10:44:36 +0000

    - Update to brand New Hear (2017).

--- src/qtractor.cpp
+++ src/qtractor.cpp
@@ -1,7 +1,7 @@
 // qtractor.cpp
 //
 /****************************************************************************
-   Copyright (C) 2005-2016, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2005-2017, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -659,3 +659,4 @@ int main ( int argc, char **argv )
 
 
 // end of qtractor.cpp
+
