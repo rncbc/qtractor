41277dfeaba0e265bd7c6285387fdf8a7ea3f400
David Reyes Pucheta <davidpucheta@gmail.com>
2019-10-14 10:22:38 -0500

    Fix punctuation in MidiEditorForm

--- src/translations/qtractor_es.ts
+++ src/translations/qtractor_es.ts
@@ -8572,7 +8572,7 @@ Duración:	%3</translation>
     <message>
         <location filename="../qtractorMidiEditorForm.cpp" line="183"/>
         <source>Reset time-sig.</source>
-        <translation>Reiniciar compás</translation>
+        <translation type="unfinished">Reiniciar compás.</translation>
     </message>
     <message>
         <location filename="../qtractorMidiEditorForm.cpp" line="214"/>
