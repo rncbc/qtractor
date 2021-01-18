90587fe3817202b25fc19f746626929906dd3469
rncbc <rncbc@rncbc.org>
2021-01-18 09:47:08 +0000

    - CMake build system update (cmake >= 3.10)

--- README.cmake
+++ README.cmake
@@ -22,7 +22,7 @@ Linux and other systems at:
 How to use it?
 ==============
 
-1. You need CMake 3.1 or newer to build Qtractor
+1. You need CMake 3.10 or newer to build Qtractor
 
 2. Unpack the Qtractor sources somewhere, or checkout the repository, 
    and create a build directory. For instance, using a command line shell:
