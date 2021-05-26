2f0f25a4633ee1e537098266fa71351720f9b733
rncbc <rncbc@rncbc.org>
2021-05-26 22:54:37 +0100

    - Force QT_QPA_PLATFORM=xcb to standalone GUI aplications.

--- src/qtractor.cpp
+++ src/qtractor.cpp
@@ -1,7 +1,7 @@
 // qtractor.cpp
 //
 /****************************************************************************
-   Copyright (C) 2005-2020, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2005-2021, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -478,6 +478,7 @@ int main ( int argc, char **argv )
 	::signal(SIGBUS,  stacktrace);
 #endif
 #endif
+	::setenv("QT_QPA_PLATFORM", "xcb", 0);
 #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
 #if QT_VERSION <  QT_VERSION_CHECK(6, 0, 0)
 	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
