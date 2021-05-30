68b959df330e5fee2a6c1d1ccb99c11af4dc6665
rncbc <rncbc@rncbc.org>
2021-05-30 16:13:34 +0100

    - Force QT_QPA_PLATFORM=xcb to GUI aplications (GNU/Linux only).

--- src/qtractor.cpp
+++ src/qtractor.cpp
@@ -478,7 +478,9 @@ int main ( int argc, char **argv )
 	::signal(SIGBUS,  stacktrace);
 #endif
 #endif
+#if defined(Q_OS_LINUX)
 	::setenv("QT_QPA_PLATFORM", "xcb", 0);
+#endif
 #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
 #if QT_VERSION <  QT_VERSION_CHECK(6, 0, 0)
 	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
