4a040904bf74091eebadd966830db4c382381119
rncbc <rncbc@rncbc.org>
2022-05-23 09:04:34 +0100

    - Fixed misused delete[]...

--- src/qtractorAudioClip.cpp
+++ src/qtractorAudioClip.cpp
@@ -1,7 +1,7 @@
 // qtractorAudioClip.cpp
 //
 /****************************************************************************
-   Copyright (C) 2005-2021, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2005-2022, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -887,7 +887,7 @@ bool qtractorAudioClip::clipExport ( ClipExport pfnClipExport, void *pvArg,
 	}
 
 	for (i = 0; i < iChannels; ++i)
-		delete ppFrames[i];
+		delete [] ppFrames[i];
 	delete [] ppFrames;
 	delete pBuff;
 
