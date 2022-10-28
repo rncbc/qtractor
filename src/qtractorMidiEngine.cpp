639e75d4407de9a217b574efb790fb83abd41908
rncbc <rncbc@rncbc.org>
2022-10-28 22:15:34 +0100

    - Removed some redundant MIDI queue time drift correction code.

--- src/qtractorMidiEngine.cpp
+++ src/qtractorMidiEngine.cpp
@@ -2188,14 +2188,14 @@ void qtractorMidiEngine::driftCheck (void)
 		const long iMidiTime
 			= long(snd_seq_queue_status_get_tick_time(pQueueStatus));
 		long iDeltaTime = (iAudioTime - iMidiTime);
-	//	if (pSession->isLooping()) {
+	#if 0//if (pSession->isLooping()) {
 			const long iDeadTime
 				= pSession->tickFromFrame(iAudioFrame + readAhead())
 				- iAudioTime - m_iTimeStart;
 			const long iDeadTime2 = long(iDeadTime >> 4);
 			if (iDeltaTime < -iDeadTime2 || iDeltaTime > +iDeadTime2)
 				iDeltaTime = 0;
-	//	}
+	#endif//}
 		if (iDeltaTime && iAudioTime > 0 && iMidiTime > 0)
 			m_iTimeDrift += (iDeltaTime << 1);
 		if (m_iTimeDrift && (iAudioTime > -m_iTimeDrift)) {
