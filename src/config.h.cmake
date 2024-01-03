b8386fa1fb16823a591a74293c95a361f71d8119
rncbc <rncbc@rncbc.org>
2024-01-03 09:05:59 +0000

    - Updated copyright headers into the New Year. (final)

--- src/config.h.cmake
+++ src/config.h.cmake
@@ -16,6 +16,13 @@
 /* Define to the homepage of this package. */
 #cmakedefine PROJECT_HOMEPAGE_URL "@PROJECT_HOMEPAGE_URL@"
 
+/* Define to the copyright of this package. */
+#cmakedefine PROJECT_COPYRIGHT "@PROJECT_COPYRIGHT@"
+
+/* Define to the domain of this package. */
+#cmakedefine PROJECT_DOMAIN "@PROJECT_DOMAIN@"
+
+
 /* Default installation prefix. */
 #cmakedefine CONFIG_PREFIX "@CONFIG_PREFIX@"
 
@@ -219,5 +226,6 @@
 
 /* Define if Wayland is supported (NOT RECOMMENDED) */
 #cmakedefine CONFIG_WAYLAND @CONFIG_WAYLAND@
+
 
 #endif /* CONFIG_H */
