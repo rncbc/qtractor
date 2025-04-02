822c330cff1f1337c5ac4421e61f484a158b48de
rncbc <rncbc@rncbc.org>
2025-04-02 10:23:10 +0100

    - Fixed non-zero clip offset conversion on tempo(BPM) time-scale   changes. (EXPERIMENTAL)

--- src/qtractorClip.cpp
+++ src/qtractorClip.cpp
@@ -1,7 +1,7 @@
 // qtractorClip.cpp
 //
 /****************************************************************************
-   Copyright (C) 2005-2024, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2005-2025, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -317,7 +317,7 @@ void qtractorClip::updateClipTime (void)
 	m_iClipStart = pSession->frameFromTick(m_iClipStartTime);
 	m_iClipLength = pSession->frameFromTickRange(
 		m_iClipStartTime, m_iClipStartTime + m_iClipLengthTime);
-#if 1// FIXUP: Don't quantize to MIDI metronomic time-scale...
+#if 0// FIXUP: Don't quantize to MIDI metronomic time-scale...
 	m_iClipOffsetTime = pSession->tickFromFrameRange(
 		m_iClipStart, m_iClipStart + m_iClipOffset, true);
 #else
