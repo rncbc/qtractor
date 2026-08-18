bd7da161b64dfc0a0fa3b8ee788c358fa04db44c
rncbc <rncbc@rncbc.org>
2026-08-18 20:57:21 +0100

    - lupdate'd translations.

--- src/translations/qtractor_zh_CN.ts
+++ src/translations/qtractor_zh_CN.ts
@@ -832,7 +832,7 @@ Track: &quot;%1&quot; Input: &quot;%2&quot; Output: &quot;%3&quot;</source>
     <message>
         <location filename="../qtractorClapPlugin.cpp" line="560"/>
         <location filename="../qtractorVst2Plugin.cpp" line="663"/>
-        <location filename="../qtractorVst3Plugin.cpp" line="1377"/>
+        <location filename="../qtractorVst3Plugin.cpp" line="1582"/>
         <source>Vendor: </source>
         <translation>厂商：</translation>
     </message>
@@ -849,7 +849,7 @@ Track: &quot;%1&quot; Input: &quot;%2&quot; Output: &quot;%3&quot;</source>
     <message>
         <location filename="../qtractorClapPlugin.cpp" line="544"/>
         <location filename="../qtractorVst2Plugin.cpp" line="671"/>
-        <location filename="../qtractorVst3Plugin.cpp" line="1350"/>
+        <location filename="../qtractorVst3Plugin.cpp" line="1555"/>
         <source>Version: </source>
         <translation>版本：</translation>
     </message>
@@ -948,17 +948,17 @@ Track: &quot;%1&quot; Input: &quot;%2&quot; Output: &quot;%3&quot;</source>
     </message>
     <message>
         <location filename="../qtractorClapPlugin.cpp" line="538"/>
-        <location filename="../qtractorVst3Plugin.cpp" line="1344"/>
+        <location filename="../qtractorVst3Plugin.cpp" line="1549"/>
         <source>Name: </source>
         <translation>名称：</translation>
     </message>
     <message>
-        <location filename="../qtractorVst3Plugin.cpp" line="1364"/>
+        <location filename="../qtractorVst3Plugin.cpp" line="1569"/>
         <source>Category: </source>
         <translation>类别：</translation>
     </message>
     <message>
-        <location filename="../qtractorVst3Plugin.cpp" line="1371"/>
+        <location filename="../qtractorVst3Plugin.cpp" line="1576"/>
         <source>Categories: </source>
         <translation>类别：</translation>
     </message>
